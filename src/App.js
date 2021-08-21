7e983f662fbdae0928f7ed6525f573a5b56853a5
Kartikeya Choudhary <89142612+kart-c@users.noreply.github.com>
2021-08-21 21:52:35 +0530

    feat Added setState feature to make the app interactive

--- src/App.js
+++ src/App.js
@@ -16,22 +16,46 @@ const foodEmojis = {
 };
 
 export default function App() {
+  const [text, setText] = useState("");
   const emojis = Object.keys(foodEmojis);
+
+  const emojiClickHandler = (emoji) => {
+    emojis.map((e) => {
+      if (e === emoji) {
+        setText(foodEmojis[e]);
+      }
+    });
+  };
+
+  const inputChangeHandler = (e) => {
+    const inputValue = e.target.value;
+    emojis.map((emoji) => {
+      if (inputValue === emoji) {
+        setText(foodEmojis[inputValue]);
+      }
+    });
+  };
+
   return (
     <div className="App">
-      <h1>Welcome to Know your food Emojis</h1>
+      <h1>Welcome to Know your food</h1>
 
       <div>
         <input
           className="input"
           placeholder="Enter your favourite food emojis"
+          onChange={inputChangeHandler}
         ></input>
-        <div className="text-displayed"></div>
+        <div className="text-displayed">{text}</div>
       </div>
       <div className="emoji-list-tag">Emojis we Know</div>
       <ul className="emoji-list">
         {emojis.map((emoji) => {
-          return <li key={emoji}>{emoji}</li>;
+          return (
+            <li key={emoji} onClick={() => emojiClickHandler(emoji)}>
+              {emoji}
+            </li>
+          );
         })}
       </ul>
     </div>
